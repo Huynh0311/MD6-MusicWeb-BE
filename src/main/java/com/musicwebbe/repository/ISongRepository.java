4d3a0ccade6e8f3f265a93f60fba7e4edd6aa6a8
unknown <dung2003vt@gmail.com>
2023-10-10 10:46:10 +0700

    done

--- src/main/java/com/musicwebbe/repository/ISongRepository.java
+++ src/main/java/com/musicwebbe/repository/ISongRepository.java
@@ -2,6 +2,9 @@ package com.musicwebbe.repository;
 
 import com.musicwebbe.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ISongRepository extends JpaRepository<Song,Integer> {
 }
